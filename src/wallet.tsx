cb2f30e813524a3c406674f8b6132953e06746de
xd-Abi <abinayansureskumar@gmail.com>
2024-10-14 14:00:00 +0200

    Created a custom smoldot light node and connecting to it

--- src/wallet.tsx
+++ src/wallet.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import {useEffect, useState} from "react";
+import {ScProvider} from "@polkadot/rpc-provider/substrate-connect";
+import * as Sc from "@substrate/connect";
+import {ApiPromise} from "@polkadot/api";
 
 export function Wallet() {
-  return <div></div>;
+  useEffect(() => {
+    const initClient = async () => {
+      const provider = new ScProvider(Sc, Sc.WellKnownChain.polkadot);
+      await provider.connect();
+      const api = await ApiPromise.create({provider});
+
+      await api.rpc.chain.subscribeNewHeads(lastHeader => {
+        console.log(`New block hash: ${lastHeader.hash.toHex()}`);
+      });
+    };
+
+    initClient().catch(console.error);
+  }, []);
+
+  return <div>HELLO</div>;
 }
